0e5e46aeec9ceecd18ff7b191f660d173b540432
dingwenxiao <stephen150150@gmail.com>
2017-06-22 21:16:46 -0400

    rename

--- src/main/java/test/rest_practice/controller/RestController.java
+++ src/main/java/test/rest_practice/controller/RestController.java
@@ -6,31 +6,22 @@ import java.util.concurrent.atomic.AtomicLong;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
 
-import test.rest_practice.model.Greeting;
 import test.rest_practice.model.Record;
 import test.rest_practice.service.RecordService;
 
-@RestController
-public class GreetingController {
-
-  private static final String template = "Hello, %s!";
-  private final AtomicLong counter = new AtomicLong();
+@Controller
+public class RestController {
 
   @Autowired
   RecordService recordService;
 
-  @RequestMapping("/greeting")
-  public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
-    return new Greeting(counter.incrementAndGet(), String.format(template, name));
-  }
 
   @RequestMapping(value = "/getAllRecords", method = RequestMethod.GET)
   public List<Record> getAllRecords() {
